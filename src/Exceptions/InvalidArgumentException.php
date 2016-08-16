c3eef7d51d7d5c724b2531f2eebcbd7817076f52
Amir <amir@creativeinvestments.co.uk>
2016-08-16 12:11:21 +0100

    Implemendted beta version of SphinxSE

---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,3 @@
+<?php namespace Citco\Exceptions;
+
+class SphinxSEException extends \Exception {}
--- src/SphinxSE.php
+++ src/SphinxSE.php
@@ -1,6 +1,272 @@
 <?php namespace Citco;
 
+use Citco\Exceptions\SphinxSEException;
+
 class SphinxSE
 {
+    private $query;
+	private $limit;
+	private $offset;
+	private $mode;
+	private $sort;
+	private $index;
+	private $field_weights;
+	private $filter;
+	private $range;
+	private $floatrange;
+	private $groupby;
+	private $groupsort;
+	private $host;
+	private $port;
+	private $ranker;
+	private $maxmatches;
 
+    public function __construct($config = [])
+    {
+	    foreach ($config as $key => $value)
+	    {
+		    if (property_exists($this, $key))
+		    {
+		    	$this->{$key} = $value;
+		    }
+        }
+    }
+
+	/**
+	 * Set searchd host name and port
+	 *
+	 * @param string  $host
+	 * @param integer $port
+	 */
+	public function setServer($host, $port = 0)
+	{
+		$this->host = $host;
+		$this->port = $port;
+	}
+
+	/**
+	 * Set offset and count into result set, optionally set max-matches and cutoff limits
+	 *
+	 * @param integer $offset
+	 * @param integer $limit
+	 * @param integer $max
+	 * @param integer $cutoff
+	 */
+	public function setLimits($offset, $limit, $max = 1000, $cutoff = 0)
+	{
+		$this->offset = $offset;
+		$this->limit = $limit;
+		$this->maxmatches = $max;
+	}
+
+	/**
+	 * Set maximum query time, in milliseconds, per-index. 0 means "do not limit"
+	 *
+	 * @param integer $max
+	 */
+	public function setMaxQueryTime($max)
+	{
+		$this->maxquerytime = $max;
+	}
+
+	/**
+	 * Set matching mode
+	 *
+	 * @param integer $mode
+	 *
+	 */
+	public function setMatchMode($mode)
+	{
+		$this->mode = $mode;
+	}
+
+	/**
+	 * Set ranking mode
+	 *
+	 * @param string $ranker
+	 * @param string  $rankexpr
+	 *
+	 */
+	public function setRankingMode($ranker, $rankexpr = '')
+	{
+		$this->ranker = $rankexpr ? $ranker . ':' . $rankexpr : $ranker;
+	}
+
+	/**
+	 * Set matches sorting mode
+	 *
+	 * @param string $mode
+	 * @param string  $sortby
+	 *
+	 */
+	public function setSortMode($mode, $sortby = '')
+	{
+		$this->sort = $mode . ':' . $sortby;
+	}
+
+	/**
+	 * Bind per-field weights by name
+	 *
+	 * @param array $weights
+	 *
+	 */
+	public function setFieldWeights(array $weights)
+	{
+		foreach ($weights as $field => $weight)
+		{
+			$this->field_weights .= $field . ',' . $weight . ',';
+		}
+
+		$this->field_weights = trim($this->field_weights, ',');
+	}
+
+	/**
+	 * Set index to use for search
+	 *
+	 * @param $index
+	 */
+	public function setIndex($index)
+	{
+		$this->index = $index;
+	}
+
+    public function __call($name, $arguments)
+    {
+    	if (empty($arguments))
+	    {
+	    	throw new SphinxSEException('Need to provide field query');
+	    }
+
+	    $this->fieldQuery($name, $arguments[0]);
+    }
+
+	/**
+	 * Add keywords to search on specifid fields.
+	 *
+	 * @param $fields
+	 * @param $value
+	 * @param float $quorum
+	 */
+	public function fieldQuery($fields, $value, $quorum = 0.8)
+	{
+		if (is_array($fields))
+		{
+			$field_string = '@(' . implode(',', $fields) . ')';
+		}
+		else
+		{
+			$field_string = '@' . $fields;
+		}
+
+		$this->query .= $field_string . ' "' . $this->escapeString($value) . '"/' . $quorum . ' ';
+	}
+
+	/**
+	 * Set values filter; only match records where $attribute value is in (or not in) the given set
+	 *
+	 * @param string  $attribute attribute name
+	 * @param array   $values    value set
+	 */
+	public function setFilter($attribute, array $values)
+	{
+		$this->filter[] = $attribute . ',' . implode(',', $values);
+	}
+
+	/**
+	 * Set range filter; only match records if $attribute value between $min and $max (inclusive)
+	 *
+	 * @param string  $attribute attribute name
+	 * @param integer $min       minimum attribute value
+	 * @param integer $max       maximum attribute value
+	 *
+	 */
+	public function setFilterRange($attribute, $min, $max)
+	{
+		$this->range[] = $attribute . ",{$min},{$max}";
+	}
+
+
+	/**
+	 * Set float range filter; only match records if $attribute value between $min and $max (inclusive)
+	 *
+	 * @param string  $attribute attribute name
+	 * @param float   $min       minimum attribute value
+	 * @param float   $max       maximum attribute value
+	 */
+	public function setFilterFloatRange($attribute, $min, $max)
+	{
+		$this->floatrange[] = $attribute . ",{$min},{$max}";
+	}
+
+	/**
+	 * Set grouping attribute and function
+	 *
+	 * @param string  $attribute attribute name
+	 * @param integer $func      grouping function
+	 * @param string  $groupsort group sorting clause
+	 *
+	 * @return SphinxClient
+	 * @throws \InvalidArgumentException When attribute name, group clause or function is invalid
+	 */
+	public function setGroupBy($attribute, $func, $groupsort = '@group desc')
+	{
+		$this->groupby = $func . ':' . $attribute;
+		$this->groupsort = $groupsort;
+	}
+
+	/**
+	 * Set count-distinct attribute for group-by queries
+	 *
+	 * @param string $attribute attribute name
+	 *
+	 * @return SphinxClient
+	 * @throws \InvalidArgumentException When attribute name is invalid
+	 */
+	public function setGroupDistinct($attribute)
+	{
+		$this->distinct = $attribute;
+	}
+
+    public function toQuery()
+    {
+    	$reflection = new \ReflectionClass($this);
+
+	    $properties = $reflection->getProperties();
+
+	    $query = '';
+
+	    foreach ($properties as $property)
+	    {
+	    	if (! empty($this->{$property->name}))
+		    {
+		    	if (! is_array($this->{$property->name}))
+			    {
+				    $this->{$property->name} = [$this->{$property->name}];
+			    }
+
+			    foreach ($this->{$property->name} as $value)
+			    {
+				    $query .= $property->name . '=' . $value . ';';
+			    }
+		    }
+	    }
+
+	    return $query;
+    }
+
+
+	/**
+	 * Escapes characters that are treated as special operators by the query language parser
+	 *
+	 * @param string $string unescaped string
+	 *
+	 * @return string Escaped string.
+	 */
+	public function escapeString($string)
+	{
+		$from = array('\\', '(', ')', '|', '-', '!', '@', '~', '"', '&', '/', '^', '$', '=');
+		$to   = array('\\\\', '\(', '\)', '\|', '\-', '\!', '\@', '\~', '\"', '\&', '\/', '\^', '\$', '\=');
+
+		return str_replace($from, $to, $string);
+	}
 }
